5076ae2b9fbc94d82545e72174b30bbdc14dcc7f
alfin-akhret <alfin.akhret@gmail.com>
2015-04-29 15:54:46 +0000

    find some issues with Pole object

--- src/jump2.js
+++ src/jump2.js
@@ -12,10 +12,14 @@
     var hiScore;
     var heroGravity = 800; // set the hero gravity value
     var placedPoles;
-    var minPoleGap = 10;
-    var maxPoleGap = 100;
+    var minPoleGap = 100;
+    var maxPoleGap = 300;
     var hero;
     var pole;
+    var powerBar;
+    var powerTween;
+    var jumpPower;
+    var jumping;
     
     // PROTOTYPING
     // 1. Play constructor
@@ -29,7 +33,7 @@
         preload: function() {
             game.load.image('sky', 'assets/sky.png');
             game.load.image('hero', 'assets/ninja.png');            // load the hero character
-            game.load.image('pole', 'assets/pole.png');             // load the pole object
+            game.load.image('pole', 'assets/pole2.png');             // load the pole object
             game.load.image('powerbar', 'assets/powerbar.png');     // load the powerbar
         },
         
@@ -68,7 +72,7 @@
 	};
 	Hero.prototype = Object.create(Phaser.Sprite.prototype);                    
     Hero.prototype.add = function() {
-        Phaser.Sprite.call(this, game, this.xPos, this.yPos, "hero");  
+        Phaser.Sprite.call(this, game, this.xPos, this.yPos, "hero");
 		game.physics.enable(this, Phaser.Physics.ARCADE);
 		this.body.gravity.y = heroGravity;
 		game.add.existing(this);                                                 
@@ -81,11 +85,17 @@
     };
     Pole.prototype = Object.create(Phaser.Sprite.prototype);
     Pole.prototype.add = function(){
-        Phaser.Sprite.call(this, game, this.xPos, this.yPos, "pole");
-        game.physics.enable(this, Phaser.Physics.ARCADE);
-        this.body.immovable = true;
-		game.add.existing(this);
-		platforms.add(this);
+        if(this.xPos < game.width * 2) {
+            
+            Phaser.Sprite.call(this, game, this.xPos, this.yPos, "pole");
+            game.physics.enable(this, Phaser.Physics.ARCADE);
+            this.body.immovable = true;
+    		game.add.existing(this);
+    		platforms.add(this);
+    		var nextPolePosition = this.xPos + game.rnd.between(minPoleGap, maxPoleGap);
+    		this.add(nextPolePosition);
+    
+        }
     }
     
     
@@ -96,7 +106,25 @@
     game.state.start("Play");
     
     function prepareToJump(){
-        console.log('yes');
+        // if hero is not in the air
+        if(hero.body.velocity.y == 0){
+            powerBar = game.add.sprite(hero.x, hero.y-20, "powerbar");
+            powerBar.width = 0;
+            powerTween = game.add.tween(powerBar).to({
+                width:100
+            }, 1000, "Linear", true);
+            game.input.onDown.remove(prepareToJump, this);
+            game.input.onUp.add(jump, this);
+        }
     }
 
+    function jump(){
+        jumpPower = -powerBar.width * 3 - 100;
+        powerBar.destroy();
+        game.tweens.removeAll();
+        hero.body.velocity.y = jumpPower * 2;
+        jumping = true;
+        powerTween.stop();
+        game.input.onUp.remove(jump, this);
+    }
     
